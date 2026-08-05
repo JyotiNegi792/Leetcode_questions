f55a875c892eac50203946e844014cf9a466396a
JyotiNegi <jn503971@gmail.com>
2026-08-05 13:08:00 +0530

    Time: 54 ms (92.31%) | Memory: 283.3 MB (61.54%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : invocations) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+
+        // Find all suspicious methods reachable from k
+        boolean[] suspicious = new boolean[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(k);
+        suspicious[k] = true;
+
+        while (!stack.isEmpty()) {
+            int u = stack.pop();
+            for (int v : graph.get(u)) {
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    stack.push(v);
+                }
+            }
+        }
+
+        // Check if any non-suspicious method invokes a suspicious one
+        for (int[] edge : invocations) {
+            int from = edge[0];
+            int to = edge[1];
+            if (!suspicious[from] && suspicious[to]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+
+        // Remove all suspicious methods
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
